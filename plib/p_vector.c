d4437e7f9a2b76fb70ca05188f58077c15410ec9
Stanislas Marquis <smarquis@astrorigin.ch>
2014-08-21 00:38:29 +0200

    Make it compile with P_NO_MEMPOOL defined.

--- plib/p_vector.c
+++ plib/p_vector.c
@@ -271,9 +271,9 @@ p_vector_test_csfn( p_Vector* v,
 P_INT32
 p_vector_test( P_VOID )
 {
-    
-
+#ifndef P_NO_MEMPOOL
     p_MemPool mp;
+#endif
     struct p_vector_test_t data[] =
     {
     { 1, 2 },
@@ -282,31 +282,31 @@ p_vector_test( P_VOID )
     };
     int i;
     p_Vector vec;
-    
+#ifndef P_NO_MEMPOOL
     p_mempool_set( &mp );
     p_mempool_init( &mp, 0 );
-    
+#endif
     p_vector_init( &vec, 3, sizeof( struct p_vector_test_t ), &p_vector_test_csfn );
     P_ASSERT( vec.len == 0 )
     P_ASSERT( vec.capacity == 3 * sizeof( struct p_vector_test_t ) * 2 )
-    
+
     p_vector_fill( &vec, data, 3 );
     P_ASSERT( vec.len == 3 )
     p_vector_debug( &vec );
-    
+
     for ( i = 0; i < 3; i++ )
     {
         P_PTR p = p_vector_get( &vec, i );
         P_ASSERT( p )
         P_ASSERT( ((struct p_vector_test_t*)p)->v1 == (i+(i+1)) )
     }
-    
+
     p_vector_fini( &vec );
-    
+#ifndef P_NO_MEMPOOL
     p_mempool_debug( &mp );
     p_mempool_fini( &mp );
     p_mempool_debug( &mp );
-    
+#endif
     return 0;
 }
 
